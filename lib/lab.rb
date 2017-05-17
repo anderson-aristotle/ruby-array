31a05e10fb386c382afedb5734ebbe25b81444ce
jordanallain <jallain@gmail.com>
2017-05-17 11:10:14 -0400

    Make array variables constants in lab.rb
    - Remove instruction to display result to console
    - Make variables that hold arrays constants so they can be exported

--- lib/lab.rb
+++ lib/lab.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-## Lab ##
 
-working_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+## Lab ##
+WORKING_ARRAY = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 # Append `20` to the end of the array using the array's length.
 
@@ -16,8 +16,6 @@ working_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 ## Bonus ##
 # In lib/lab.rb use a block initializer with Array.new to create
 # a Ruby Array with ten elements where elements are equal to their
-# index multiplied by 2. Store the result and display it on the
-# console with puts <array name>.
+# index multiplied by 2. Store the result in bonus_array
 
-# binding.pry
-# ""
+# BONUS_ARRAY =
